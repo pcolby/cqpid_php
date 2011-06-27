e75c8b47e5d6d38e78b7340613984b5f870d81b3
Paul Colby <git@colby.id.au>
2011-06-27 09:31:33 +0000

    Added some stdout ouput to the update.sh script.

--- update.sh
+++ update.sh
@@ -10,13 +10,17 @@ OWN_DIR=`dirname "$SELF"`
 QPID_DIR=`realpath $1`
 
 # Remove all destination files the cqpid_php repository.
+echo "Removing all destination files..."
 find "$OWN_DIR" -type f | grep -v '\.svn' | grep -v 'update.sh' | xargs rm -f
 
 cd "$QPID_DIR"
 # Copy all changed files from the Qpid source to the qpid_php repository.
+echo "Copying all new / modified files from $QPID_DIR..."
 svn status | grep -v '^?' | cut -b9- | xargs -I{} cp "{}" "../cqpid_php/{}" 2>&1 | grep -v 'omitting directory'
 # Re-create the cqpid_php diff file.
+echo "Generating the cqpid_php.diff file..."
 svn diff > "$OWN_DIR/cqpid_php.diff"
 
 # Copy any other dependencies to the cqpid_php checkout.
+echo "Updating the FindPHPDev.cmake file..."
 cp '/usr/share/cmake-2.8/Modules/FindPHPDev.cmake' "$OWN_DIR"
