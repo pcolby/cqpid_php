baeda821e8975414ea30dcfab511a506d3bf6d85
Paul Colby <git@colby.id.au>
2011-09-21 10:57:49 +0000

    A minor fix to the update.sh script.

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ find "$OWN_DIR" -type f | grep -v '\.svn' | grep -v 'update.sh' | xargs rm -f
 cd "$QPID_DIR"
 # Copy all changed files from the Qpid source to the qpid_php repository.
 echo "Copying all new / modified files from $QPID_DIR..."
-svn status | grep -v '^?' | cut -b9- | xargs -I{} cp "{}" "../cqpid_php/{}" 2>&1 | grep -v 'omitting directory'
+svn status | grep -v '^?' | cut -b9- | xargs -I{} cp '{}' "$OWN_DIR/{}" 2>&1 | grep -v 'omitting directory'
 # Re-create the cqpid_php diff file.
 echo "Generating the cqpid_php.diff file..."
 svn diff > "$OWN_DIR/cqpid_php.diff"
@@ -24,3 +24,5 @@ svn diff > "$OWN_DIR/cqpid_php.diff"
 # Copy any other dependencies to the cqpid_php checkout.
 echo "Updating the FindPHPDev.cmake file..."
 cp '/usr/share/cmake-2.8/Modules/FindPHPDev.cmake' "$OWN_DIR"
+
+# Tip: patch -p0 [--dry-run] < cqpid_php.diff
